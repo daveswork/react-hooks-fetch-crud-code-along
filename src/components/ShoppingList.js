e74f9193ee7d129933965c15f7c39f5a3c3f6268
Dave J <29786887+daveswork@users.noreply.github.com>
2024-08-14 07:19:29 -0400

    Completed Updated items.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -18,6 +18,18 @@ function ShoppingList() {
       setItems([...items, newItem])
     }
 
+    function handleUpdatedItem(updatedItem){
+      console.log("In ShoppingCart:", updatedItem)
+      const updatedItems = items.map((item) => {
+        if (item.id === updatedItem.id){
+          return updatedItem;
+        } else {
+          return item;
+        }
+      })
+      setItems(updatedItems)
+    }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
@@ -37,7 +49,7 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdatedItem={handleUpdatedItem}/>
         ))}
       </ul>
     </div>
